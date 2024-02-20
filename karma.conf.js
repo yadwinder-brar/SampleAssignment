dc3ef468cd8b080531b49257603ba4b02d40d34f
yadwinder <yadibrar7262@gmail.com>
2024-02-20 11:14:36 +0530

    basic setup

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ship-district'),
+      dir: require('path').join(__dirname, './coverage/assignment'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ship-district'`, () => {
+  it(`should have as title 'assignment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ship-district');
+    expect(app.title).toEqual('assignment');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ship-district app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('assignment app is running!');
   });
 });
